ddd35c7ed45c93e5eb786985a8b26eeeffa34835
MariaAndrade1234 <beatrizfelixandrade@gmail.com>
2025-11-19 08:03:05 -0300

    feat: implementa user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from users.models import CustomUser
+from users.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        """
+        Garante que apenas usuários autenticados possam ver/editar, 
+        mas permite que qualquer um crie (POST).
+        """
+        if self.action == 'create':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+
