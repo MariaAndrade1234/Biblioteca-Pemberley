fe3ce0e1dfc2a12fa97d58d27f1a8b2575366a2e
Maria Beatriz Andrade <maria.andrade@capgemini.com>
2025-12-04 16:12:22 -0300

    chore: refactor URL routing to remove DefaultRouter and define paths explicitly

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,13 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
+from django.urls import path
 from users.views import UserViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = 'users'
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+urlpatterns = [
+	path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+	path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='user-detail'),
 
-urlpatterns = router.urls + [
 	path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
